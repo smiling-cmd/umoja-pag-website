361549ac226bc6b4598e10cba56a50ab5a7cb377
Yvonne wambura <147663626+smiling-cmd@users.noreply.github.com>
2026-08-12 14:46:54 +0300

    Back up files moved

--- _writeup_backup_20260809-152606-765010/umoja_writeup_patch_package/umoja_writeup_patch_package/run_patch.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\laragon\www\Umoja
-python apply_writeup_alignment.py .
-echo.
-echo Patch finished. Review the backup path shown above, then refresh the local site with Ctrl+F5.
-pause
